4632a914a642367b57b244d72ffaf890bf286247
KathyReid <kathy@kathyreid.id.au>
2016-12-18 23:08:14 +1100

    Milestone commit - got labels appearing

--- charts/expenses.js
+++ charts/expenses.js
@@ -4,23 +4,20 @@ var color = d3.scaleOrdinal()
     .range([
         "#ffc100",
         "#ff0000",
-        "#666666",
-        "#00288e",
-        "#CD0074",
-        "#d0f623",
-        "#7109AB",
-        "#FF7400",
         "#393939",
-        "#00CC00",
-        "#ab005d",
-        "#ffca24",
-        "#6225ad",
-        "#ff4000",
-        "#ffe501",
-        "#0A67A3",
-        "#ffa100",
-        "#E1014C",
-        "#000000"
+        "#FFCD33",
+        "#FF3333",
+        "#616161",
+        "#FFDA66",
+        "#FF6666",
+        "#888888",
+        "#FFE699",
+        "#FF9999",
+        "#B0B0B0",
+        "#FFF3CC",
+        "#FFCCCC",
+        "#D7D7D7",
+        "#fff"
       ])
     .domain([
         "Face to face",
@@ -38,20 +35,11 @@ var color = d3.scaleOrdinal()
         "Bank fees",
         "Foreign currency losses",
         "Subscriptions",
-        "Telephone and internet",
-        "Food and drink",
-        "Ghosts",
-        "Transport"
+        "Other"
     ]);
 
 
-var CollectionArcSequence = 0;
-var LoansArcSequence      = 1;
-var MembersArcSequence    = 2;
-var EventsArcSequence     = 3;
-var VisitsArcSequence     = 4;
-var WirelessArcSequence   = 5;
-var InternetArcSequence   = 6;
+var ExpensesArcSequence = 0;
 
 var widthModifier = 0.87;
 var heightModifier = 0.87;
@@ -60,57 +48,45 @@ var width = innerWidth * widthModifier,
     height = innerHeight * heightModifier,
     radius = Math.min(width, height) / 2;
 
-console.log(radius);
-
 var annularXOffset  = 100; // how much to shift the annulars horizontally from centre
 var annularYOffset  = 0; // how much to shift the annulars vertically from centre
-var annularWidth    = 150; // width of each annular
+var annularWidth    = 140; // width of each annular
 var annularSpacing  = 15;
 var annularMargin   = 200; // margin between annulars and canvas
-var padAngle        = 0.035; // amount that each segment of an annular is padded
-var cornerRadius    = 1; // amount that the sectors are rounded
+var padAngle        = 0.03; // amount that each segment of an annular is padded
+var cornerRadius    = 5; // amount that the sectors are rounded
 
-var titleOffSet     = 30; // amount in pixels that the title arc inner and outer radii are offset from the dataset arc that they are labelling
-var titleRotationOffSet = -14; // amount in percent that the title is rotated from the 180 degree mark
+var titleOffSet     = 22; // amount in pixels that the title arc inner and outer radii are offset from the dataset arc that they are labelling
+var titleRotationOffSet = -25; // amount in percent that the title is rotated from the 180 degree mark
 var endAngle        = 0; // end angle for title arcs
-
-/* Variables used in displaying the legend */
-var legendWidth       = 40; // size of each rectangle for the legend
-var legendHeight      = 35; // size of each rectangle for the legend
-var legendXSpacing    = 7; // x offset for series values
-var legendYSpacing    = 10;  // y offset for series values
-var legendPlacementX  = (width / 2) - (radius) // x co-ordinate for legend placement
-var legendPlacementY  = ((height/2*-1) + (annularMargin/3)); // y co-ordinate for legend placement
-var legendStrokeColor = '#000';
-
-/* Variables used for the labels inside the annular */
-var labelOffset = radius/100*60; // number of pixels inside each segment that the label is placed
 
 /* Variables used with the d3.tip tooltips */
 
 var tipXOffSet = 0; // x offset for tooltip display in pixels
 var tipYOffSet = 0; // y offset for tooltip display in pixels
 
+/* Variables used with the labels */
+
+var labelcxOffset = 200;
+var labelxOffset  = -20;
+var labelcyOffset = 200;
+var labelyOffset  = -20;
+
 /*
   Main Annular
 */
 
 var ExpensesArc = d3.arc()
-    .outerRadius(radius - (annularMargin + ((CollectionArcSequence - 1) *   annularWidth + ((CollectionArcSequence - 1) * annularSpacing))))
-    .innerRadius(radius - (annularMargin + (CollectionArcSequence * annularWidth + ((CollectionArcSequence - 1) * annularSpacing))))
+    .outerRadius(radius - (annularMargin + ((ExpensesArcSequence - 1) *   annularWidth + ((ExpensesArcSequence - 1) * annularSpacing))))
+    .innerRadius(radius - (annularMargin + (ExpensesArcSequence * annularWidth + ((ExpensesArcSequence - 1) * annularSpacing))))
     .padAngle(padAngle)
     .cornerRadius(cornerRadius);
 
 var ExpensesTitleArc = d3.arc()
-    .outerRadius(radius + titleOffSet - (annularMargin + ((CollectionArcSequence - 1) *   annularWidth + ((CollectionArcSequence - 1) * annularSpacing))))
-    .innerRadius(radius - titleOffSet - (annularMargin + (CollectionArcSequence * annularWidth + ((CollectionArcSequence - 1) * annularSpacing))))
+    .outerRadius(radius + titleOffSet - (annularMargin + ((ExpensesArcSequence - 1) *   annularWidth + ((ExpensesArcSequence - 1) * annularSpacing))))
+    .innerRadius(radius - titleOffSet - (annularMargin + (ExpensesArcSequence * annularWidth + ((ExpensesArcSequence - 1) * annularSpacing))))
     .startAngle(Math.PI-(Math.PI/100*titleRotationOffSet))
     .endAngle(endAngle);
-
-var labelArc = d3.arc()
-    .outerRadius(radius - annularWidth + 100)
-    .innerRadius(radius - annularWidth - 120);
-
 
 /*
   Pie shapes for each data set
@@ -133,11 +109,9 @@ var BaseSvg = d3.select("#visualisation").append("svg")
 /*
   Layers for each annular
 */
-
 var ExpensesLayer   = BaseSvg.append('g');
-
 var TitleLayer      = BaseSvg.append('g');
-var LegendLayer     = BaseSvg.append('g');
+var LabelLayer      = BaseSvg.append('g');
 
 /*
   CSV calls for each data set
@@ -169,22 +143,81 @@ d3.csv("expenses.csv", ExpensesType, function(error, ExpensesData) {
       .on('mouseover', ExpensesTip.show)
       .on('mouseout', ExpensesTip.hide);
 
-  g.append("text")
-    .attr("transform", function(d) {
-    var midAngle = d.endAngle < Math.PI ? d.startAngle/2 + d.endAngle/2 : d.startAngle/2  + d.endAngle/2 + Math.PI ;
-    return "translate(" + labelArc.centroid(d)[0] + "," + labelArc.centroid(d)[1] + ") rotate(-90) rotate(" + (midAngle * 180/Math.PI) + ")"; })
-    .attr("class", "labelText")
-    .attr('text-anchor','middle')
-    .attr("dy", ".35em")
-    .text(function(d) { return "$" +  (d3.format(",d")(d.data.Value)) });
+
+    /*
+      Text labels and lines to each label
+    */
+
+
+
+    var ExpensesLabels = LabelLayer.selectAll("text");
+
+    ExpensesLabels.data(ExpensesPie(ExpensesData))
+    .enter().append("text")
+    .attr("text-anchor", "middle")
+    .attr("x", function(d) {
+        var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
+        d.cx = Math.cos(a) * (radius - labelcxOffset);
+        return d.x = Math.cos(a) * (radius - labelxOffset);
+    })
+    .attr("y", function(d) {
+        var a = d.startAngle + (d.endAngle - d.startAngle)/2 - Math.PI/2;
+        d.cy = Math.sin(a) * (radius - labelcyOffset);
+        return d.y = Math.sin(a) * (radius - labelyOffset);
+    })
+    .text(function(d) { return d.data.Account + ' - ' + d3.format("$d")(d.data.Value); })
+    .attr("class", "ExpensesLabels")
+    .each(function(d) {
+        var bbox = this.getBBox();
+        d.sx = d.x - bbox.width/2 - 2;
+        d.ox = d.x + bbox.width/2 + 2;
+        d.sy = d.oy = d.y + 5;
+    });
+
+    LabelLayer.append("defs").append("marker")
+    .attr("id", "circ")
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("refX", 3)
+    .attr("refY", 3)
+    .append("circle")
+    .attr("cx", 3)
+    .attr("cy", 3)
+    .attr("r", 3);
+
+    ExpensesLabels.selectAll("path.pointer")
+    .data(ExpensesData).enter()
+    .append("path")
+    .attr("class", "pointer")
+    .style("fill", "none")
+    .style("stroke", "black")
+    .attr("marker-end", "url(#circ)")
+    .attr("d", function(d) {
+        if(d.cx > d.ox) {
+            return "M" + d.sx + "," + d.sy + "L" + d.ox + "," + d.oy + " " + d.cx + "," + d.cy;
+        } else {
+            return "M" + d.ox + "," + d.oy + "L" + d.sx + "," + d.sy + " " + d.cx + "," + d.cy;
+        }
+    });
+
+
+
+
+
 
 });
+
+
+
+
+
+
 
 /*
   Add the titles to each annular
 */
 
-var ExpensesTitleText     = 'LA Non-Event Expenditure 2015-2016';
+var ExpensesTitleText     = 'Linux Australia Non-Event Expenditure 1 October 2015 - 30 September 2016';
 
 TitleLayer.append("path")
       .attr("d", ExpensesTitleArc)
@@ -195,38 +228,8 @@ TitleLayer.append("text")
       .attr("id", "ExpensesTitleText")
       .append("textPath")
       .attr("xlink:href", "#ExpensesTitle") //place the ID of the path here
-      .attr("class", "Title")
-    //  .attr("startOffset", "25%")
+      .attr("class", "ExpensesTitle")
       .text(ExpensesTitleText);
-
-/*
-  Add a Legend
-*/
-
-var legend = LegendLayer.selectAll("g")
-    .data(color.domain())
-    .enter()
-    .append('g')
-    .attr('x', legendPlacementX)
-    .attr('y', legendPlacementY)
-    .attr('class', 'legend')
-    .attr('transform', function(d, i) {
-        return 'translate(' + (legendPlacementX + legendWidth) + ',' + (legendPlacementY + (i * legendHeight)) + ')';
-});
-
-legend.append('rect')
-    .attr('width', legendWidth)
-    .attr('height', legendHeight)
-    .attr('class', 'legendRect')
-    .style('fill', color)
-    .style('stroke', legendStrokeColor);
-
-legend.append('text')
-    .attr('x', legendWidth + legendXSpacing)
-    .attr('y', legendHeight - legendYSpacing)
-    .attr('class', 'legendText')
-    .text(function(d) { return d ; }
-  );
 
 /*
   Glorious functions of deliciousness
@@ -235,4 +238,8 @@ legend.append('text')
 function ExpensesType(d) {
   d.Value = +d.Value;
   return d;
+}
+
+function midAngle(d){
+    return d.startAngle + (d.endAngle - d.startAngle)/2;
 }
