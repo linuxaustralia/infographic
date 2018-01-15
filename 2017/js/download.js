3c177132b47e6afb754bc80743bca0a4b9b773aa
KathyReid <kathy@kathyreid.id.au>
2018-01-15 18:42:25 +1100

    Update fonts

--- 2017/js/download.js
+++ 2017/js/download.js
@@ -3,7 +3,7 @@
 */
 d3.select('#download').on('click', function() {
   var config = {
-    filename: 'la-expenses-2015-2016',
+    filename: 'la-expenses-2016-2017',
   }
   d3_save_svg.save(d3.select('#BaseSvg').node(), config);
 });
